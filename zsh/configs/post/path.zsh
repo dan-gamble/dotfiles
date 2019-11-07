d4597cb8b271c826e39b82f8d479ea54450f303b
Dan Gamble <dan@dangamble.co.uk>
2019-11-07 10:05:48 +0000

    Use Postgres 11

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -1,6 +1,6 @@
 # ensure dotfiles bin directory is loaded first
 PATH="$HOME/.bin:/usr/local/sbin:$PATH"
-PATH="/Applications/Postgres.app/Contents/Versions/10/bin:$PATH"
+PATH="/Applications/Postgres.app/Contents/Versions/11/bin:$PATH"
 
 PATH=$(brew --prefix)/share/python:$(brew --prefix)/share/python/bin:$(brew --prefix)/share/python/sbin:$PATH
 PATH=$PATH:/usr/local/opt/go/libexec/bin # Go
